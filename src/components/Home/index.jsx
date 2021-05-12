822d405b897315c053f4d8f524476f60479efa63
William Judd <juddwilliam13@gmail.com>
2021-05-11 20:13:53 -0500

    working on Home component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom";
+import "./style.css";
+
 const Home = () => {
   return (
-    <div>
-      Home component
+    <div className= "home">
+      <div className= "title">
+        <h1>
+          <p>Meticulously Crafted</p>
+          <p>Full Stack</p>
+          <p>Web Design</p>
+        </h1>
+        <Link to="about">
+          <button>Hire Me</button>
+        </Link>
+      </div>
+      <div className="person">
+        <img
+        src={`${process.env.PUBLIC_URL}/portfolio-pic.png`}
+        alt="person picture"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home
