76e91d9e9056c0f08a0b25504d7a76866ef4f871
William Judd <juddwilliam13@gmail.com>
2021-05-12 22:24:49 -0500

    adding skills component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
 import NavBar from './components/NavBar';
+import Skills from './components/Skills';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/skills" component={Skills} />
         </Switch>
         <Footer/>
       </div>
---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,33 @@
+import "./style.css"
+
+const Skills = () => {
+  return (
+    <div className="skills">
+      <h1>Skills</h1>
+      <div className="content">
+        <div>
+          <strong>Languages:</strong>
+          <li>CSS3</li>
+          <li>HTML5</li>
+          <li>Node.js</li>
+          <li>JavaScript</li>
+        </div>
+        <div>
+          <strong>Libraries:</strong>
+          <li>Bootstrap</li>
+          <li>Express.js</li>
+          <li>React.js</li>
+        </div>
+        <div>
+          <strong>Tools:</strong>
+          <li>EsLint</li>
+          <li>Chrome Dev</li>
+          <li>Git & GitHub</li>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
+        
